2c0fd4f389e275187ec1de7f7a18dd2722ee0553
KoreaHanwoo <koreahan8549@gmail.com>
2022-02-05 18:55:21 +0900

    Update commands

--- src/main/java/com/hanwoo/reach/Reach.java
+++ src/main/java/com/hanwoo/reach/Reach.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.RayTraceResult;
@@ -18,7 +19,9 @@ import java.util.logging.Level;
 
 public final class Reach extends JavaPlugin implements Listener {
     private final Set<Entity> exclude = new HashSet<>();
+    private final Set<UUID> throwExclude = new HashSet<>();
     private final Map<UUID, Integer> playerReach = new HashMap<>();
+    private Integer defaultReach;
 
     @Override
     public void onEnable() {
@@ -29,6 +32,7 @@ public final class Reach extends JavaPlugin implements Listener {
         }
         if (!this.isEnabled()) return;
         saveDefaultConfig();
+        if (getConfig().contains("default")) defaultReach = getConfig().getInt("default", 0);
         for (String key : getConfig().getKeys(false)) {
             UUID uuid;
             try {
@@ -57,27 +61,39 @@ public final class Reach extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onClick(PlayerInteractEvent e) {
-        if (!hasReach(e.getPlayer().getUniqueId())) return;
         if (!(e.getAction().equals(Action.LEFT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_BLOCK))) return;
+        if (throwExclude.contains(e.getPlayer().getUniqueId())) {
+            throwExclude.remove(e.getPlayer().getUniqueId());
+            return;
+        }
+        if (!hasReach(e.getPlayer().getUniqueId()) && defaultReach == null) return;
         Player player = e.getPlayer();
         Location eyeLocation = player.getEyeLocation();
         Vector direction = eyeLocation.getDirection();
         Predicate<Entity> filter = (entity) -> !entity.equals(player);
-        RayTraceResult result = player.getWorld().rayTrace(eyeLocation, direction, getReach(player.getUniqueId()), FluidCollisionMode.NEVER, false, 0, filter);
+        RayTraceResult result = player.getWorld().rayTrace(eyeLocation, direction,
+                hasReach(e.getPlayer().getUniqueId()) ? getReach(player.getUniqueId()) : defaultReach,
+                FluidCollisionMode.NEVER, false, 0, filter);
         if (result == null) return;
-        Entity target = result.getHitEntity();
-        if (target == null) return;
-        if (exclude.contains(target))
-            exclude.remove(target);
-        else
-            Attack.attack(player, target);
-
+        Entity entity = result.getHitEntity();
+        if (entity == null) return;
+        if (exclude.contains(entity)) {
+            exclude.remove(entity);
+            return;
+        }
+        if (entity.getPassengers().contains(player)) return;
+        Attack.attack(player, entity);
     }
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
         exclude.add(e.getEntity());
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> exclude.remove(e.getEntity()));
+    }
+
+    @EventHandler
+    public void onThrow(PlayerDropItemEvent e) {
+        throwExclude.add(e.getPlayer().getUniqueId());
     }
 
     public Map<UUID, Integer> getPlayerReach() {
@@ -90,5 +106,13 @@ public final class Reach extends JavaPlugin implements Listener {
 
     public int getReach(UUID uuid) {
         return playerReach.getOrDefault(uuid, 0);
+    }
+
+    public Integer getDefaultReach() {
+        return defaultReach;
+    }
+
+    public void setDefaultReach(Integer defaultReach) {
+        this.defaultReach = defaultReach;
     }
 }
--- src/main/java/com/hanwoo/reach/ReachCommand.java
+++ src/main/java/com/hanwoo/reach/ReachCommand.java
@@ -9,9 +9,11 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReachCommand implements CommandExecutor, TabCompleter {
     private final Reach plugin;
@@ -23,30 +25,78 @@ public class ReachCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length < 1)
-            return sendMessage(sender, ChatColor.RED + "Usage: /reach <player> [(number)|reset]");
-        Player player = Bukkit.getPlayerExact(args[0]);
-        if (player == null)
-            return sendMessage(sender, ChatColor.RED + "No player was found");
-        if (args.length < 2)
-            return sendMessage(sender, plugin.hasReach(player.getUniqueId()) ?
-                    player.getName() + "'s reach distance is " + plugin.getReach(player.getUniqueId()) :
-                    player.getName() + " does not have a reach distance set");
-        if (args[1].equalsIgnoreCase("reset")) {
-            plugin.getPlayerReach().remove(player.getUniqueId());
-            plugin.getConfig().set(player.getUniqueId().toString(), null);
-            return sendMessage(sender, "Reset " + player.getName() + "'s reach distance");
+            return sendMessage(sender, ChatColor.RED + "Usage: /reach (default|player)");
+        if (args[0].equalsIgnoreCase("default")) {
+            if (args.length < 2) return sendMessage(sender, plugin.getDefaultReach() == null ?
+                    "No default reach set" :
+                    "The default reach is " + plugin.getDefaultReach()
+            );
+            if (args[1].equalsIgnoreCase("reset")) {
+                plugin.setDefaultReach(null);
+                plugin.getConfig().set("default", null);
+                return sendMessage(sender, "Reset default reach");
+            }
+            int reach;
+            try {
+                reach = Integer.parseInt(args[1]);
+            } catch (Exception e) {
+                return sendMessage(sender, ChatColor.RED + "Invalid number");
+            }
+            if (reach < 0) return sendMessage(sender, ChatColor.RED + "Reach cannot be negative");
+            if (reach > 100) return sendMessage(sender, ChatColor.RED + "Reach cannot be greater than 100");
+            plugin.setDefaultReach(reach);
+            sender.sendMessage("Set default reach to " + reach);
+            plugin.getConfig().set("default", reach);
+
+        } else if (args[0].equalsIgnoreCase("player")) {
+            if (args.length < 2)
+                return sendMessage(sender, ChatColor.RED + "Usage: /reach player <player> [number|reset]");
+            boolean wildcard = args[1].equalsIgnoreCase("*");
+            Player player = wildcard ? null : Bukkit.getPlayerExact(args[1]);
+            if (player == null && !wildcard)
+                return sendMessage(sender, ChatColor.RED + "No player was found");
+            if (args.length < 3)
+                if (wildcard)
+                    return sendMessage(sender, ChatColor.RED + "Usage: /reach player * [number|reset]");
+                else
+                    return sendMessage(sender, plugin.hasReach(player.getUniqueId()) ?
+                            player.getName() + "'s reach is " + plugin.getReach(player.getUniqueId()) :
+                            player.getName() + " does not have a reach set");
+            if (args[2].equalsIgnoreCase("reset")) {
+                if (wildcard) {
+                    plugin.getPlayerReach().clear();
+                    plugin.getConfig().getKeys(false).forEach(k -> {
+                        if (!k.equals("default")) plugin.getConfig().set(k, null);
+                    });
+                    return sendMessage(sender, "Reset everyone's reach");
+                } else {
+                    plugin.getPlayerReach().remove(player.getUniqueId());
+                    plugin.getConfig().set(player.getUniqueId().toString(), null);
+                    return sendMessage(sender, "Reset " + player.getName() + "'s reach");
+                }
+            }
+            int reach;
+            try {
+                reach = Integer.parseInt(args[2]);
+            } catch (Exception e) {
+                return sendMessage(sender, ChatColor.RED + "Invalid number");
+            }
+            if (reach < 0) return sendMessage(sender, ChatColor.RED + "Reach cannot be negative");
+            if (reach > 100) return sendMessage(sender, ChatColor.RED + "Reach cannot be greater than 100");
+            if (wildcard) {
+                plugin.getPlayerReach().forEach((key, val) -> plugin.getPlayerReach().put(key, reach));
+                plugin.getConfig().getKeys(false).forEach(k -> {
+                    if (!k.equals("default")) plugin.getConfig().set(k, reach);
+                });
+                Bukkit.getOnlinePlayers().forEach(p -> plugin.getConfig().set(p.getUniqueId().toString(), reach));
+                return sendMessage(sender, "Set everyone's reach to " + reach);
+            }
+            plugin.getPlayerReach().put(player.getUniqueId(), reach);
+            plugin.getConfig().set(player.getUniqueId().toString(), reach);
+            sender.sendMessage("Set " + player.getName() + "'s reach to " + reach);
+        } else {
+            return sendMessage(sender, ChatColor.RED + "Usage: /reach (default|player)");
         }
-        int reach;
-        try {
-            reach = Integer.parseInt(args[1]);
-        } catch (Exception e) {
-            return sendMessage(sender, ChatColor.RED + "Invalid number");
-        }
-        if (reach < 0) return sendMessage(sender, ChatColor.RED + "Reach distance cannot be negative");
-        if (reach > 100) return sendMessage(sender, ChatColor.RED + "Reach distance cannot be greater than 100");
-        plugin.getPlayerReach().put(player.getUniqueId(), reach);
-        sender.sendMessage("Set " + player.getName() + "'s reach distance to " + reach);
-        plugin.getConfig().set(player.getUniqueId().toString(), reach);
         return true;
     }
 
@@ -57,11 +107,18 @@ public class ReachCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (args.length == 1) return Bukkit.getOnlinePlayers().stream()
-                .map(HumanEntity::getName)
-                .filter(n -> n.toLowerCase().startsWith(args[0].toLowerCase()))
-                .collect(Collectors.toList());
-        if (args.length == 2) return Collections.singletonList("reset");
+        if (args.length == 2 && args[0].equalsIgnoreCase("default")) return Collections.singletonList("reset");
+        if (args.length == 2 && args[0].equalsIgnoreCase("player"))
+            return Stream.concat(
+                            Bukkit.getOnlinePlayers().stream()
+                                    .map(HumanEntity::getName)
+                                    .filter(n -> n.toLowerCase().startsWith(args[1].toLowerCase())),
+                            Stream.of("*")
+                                    .filter(n -> args[1].isEmpty()))
+                    .collect(Collectors.toList());
+        if (args.length == 3 && args[0].equalsIgnoreCase("player")) return Collections.singletonList("reset");
+        if (args.length == 1)
+            return Stream.of("default", "player").filter(s -> s.toLowerCase().startsWith(args[0].toLowerCase())).collect(Collectors.toList());
         return Collections.emptyList();
     }
 }
